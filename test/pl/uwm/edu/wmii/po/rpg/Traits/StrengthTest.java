3e602c8803df1506b9180847c1f7557cbbe12263
Jacek Palczewskli <jpalczewski@gmail.com>
2020-11-20 18:37:03 +0100

    refactor: changes due to maven updates

--- test/pl/uwm/edu/wmii/po/rpg/Traits/StrengthTest.java
+++ test/pl/uwm/edu/wmii/po/rpg/Traits/StrengthTest.java
@@ -2,7 +2,7 @@ package pl.uwm.edu.wmii.po.rpg.Traits;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StrengthTest {
 
